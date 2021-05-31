a4c650a90c93164fcff74b59eb9c62fa2b1e2892
Desarrollo-NCA <desarrollo.nca@gmail.com>
2021-05-31 11:54:52 -0300

    Desktop

--- js/functions.js
+++ js/functions.js
@@ -9,7 +9,8 @@ for(element of navElement){
 
 var num = 0
 const nextProject = () => {
-	switch (num) {
+	if(window.matchMedia("(max-width: 750px)").matches){
+		switch (num) {
 		case 0:
 			document.getElementById('project1').classList.remove('showProject')
 			document.getElementById('project2').classList.add('showProject')
@@ -28,27 +29,101 @@ const nextProject = () => {
 			document.getElementById('project3').classList.remove('showProject')
 			num = 0
 			break;		
+		}
+	}else{
+		switch (num) {
+		case 0:
+			document.getElementById('project1').classList.remove('order1')
+			document.getElementById('project1').classList.add('order3')
+			document.getElementById('project2').classList.add('order1')
+			document.getElementById('project2').classList.remove('order2')
+			document.getElementById('project3').classList.remove('order3')
+			document.getElementById('project3').classList.add('order2')
+			num = 1
+			break;
+		case 1:
+			document.getElementById('project1').classList.remove('order3')
+			document.getElementById('project1').classList.add('order2')
+			document.getElementById('project2').classList.remove('order1')
+			document.getElementById('project2').classList.add('order3')
+			document.getElementById('project3').classList.remove('order3')
+			document.getElementById('project3').classList.add('order1')
+			num = 2
+			break;
+		case 2:
+			document.getElementById('project1').classList.remove('order2')
+			document.getElementById('project1').classList.add('order1')
+			document.getElementById('project2').classList.remove('order3')
+			document.getElementById('project2').classList.add('order2')
+			document.getElementById('project3').classList.remove('order1')
+			document.getElementById('project3').classList.add('order3')
+			num = 0
+			break;		
+		}
 	}
 }
 const backProject = () => {
-	switch (num) {
+	if(window.matchMedia("(max-width: 750px)").matches){
+		switch (num) {
+			case 0:
+				document.getElementById('project1').classList.remove('showProject')
+				document.getElementById('project2').classList.remove('showProject')
+				document.getElementById('project3').classList.add('showProject')
+				num = 2
+				break;
+			case 1:
+				document.getElementById('project1').classList.add('showProject')
+				document.getElementById('project2').classList.remove('showProject')
+				document.getElementById('project3').classList.remove('showProject')
+				num = 0
+				break;
+			case 2:
+				document.getElementById('project1').classList.remove('showProject')
+				document.getElementById('project2').classList.add('showProject')
+				document.getElementById('project3').classList.remove('showProject')
+				num = 1
+				break;		
+		}
+	}else{
+			switch (num) {
 		case 0:
-			document.getElementById('project1').classList.remove('showProject')
-			document.getElementById('project2').classList.remove('showProject')
-			document.getElementById('project3').classList.add('showProject')
+			document.getElementById('project1').classList.remove('order1')
+			document.getElementById('project1').classList.add('order3')
+			document.getElementById('project2').classList.add('order1')
+			document.getElementById('project2').classList.remove('order2')
+			document.getElementById('project3').classList.remove('order3')
+			document.getElementById('project3').classList.add('order2')
+			
 			num = 2
 			break;
 		case 1:
-			document.getElementById('project1').classList.add('showProject')
-			document.getElementById('project2').classList.remove('showProject')
-			document.getElementById('project3').classList.remove('showProject')
+			document.getElementById('project1').classList.remove('order2')
+			document.getElementById('project1').classList.add('order1')
+			document.getElementById('project2').classList.remove('order3')
+			document.getElementById('project2').classList.add('order2')
+			document.getElementById('project3').classList.remove('order1')
+			document.getElementById('project3').classList.add('order3')
 			num = 0
 			break;
 		case 2:
-			document.getElementById('project1').classList.remove('showProject')
-			document.getElementById('project2').classList.add('showProject')
-			document.getElementById('project3').classList.remove('showProject')
+			document.getElementById('project1').classList.remove('order3')
+			document.getElementById('project1').classList.add('order2')
+			document.getElementById('project2').classList.remove('order1')
+			document.getElementById('project2').classList.add('order3')
+			document.getElementById('project3').classList.remove('order3')
+			document.getElementById('project3').classList.add('order1')
+			
 			num = 1
 			break;		
-	}
+		}
+	}	
 }
+
+window.onscroll = function() {
+  var y = window.scrollY;
+  if(y > 2000){
+  	document.getElementById('logoAirhitect').src="img/logoAirhitectWhite.png"
+  }else {
+  	document.getElementById('logoAirhitect').src="img/logoAirhitect.png"
+  }
+};
